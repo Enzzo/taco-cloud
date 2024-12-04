bcead99191365401f34750c8c2174ab189abd491
unknown <enzzomatrix@gmail.com>
2024-12-04 16:21:16 +1100

    upd

--- src/main/java/ru/vasilev/entity/Taco.java
+++ src/main/java/ru/vasilev/entity/Taco.java
@@ -2,10 +2,17 @@ package ru.vasilev.entity;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	public String name;
+	
+	@NotNull
+	@Size(min=1, message="You must choose at least 1 ingredient")
 	public List<Ingredient> ingredients;	
 }
