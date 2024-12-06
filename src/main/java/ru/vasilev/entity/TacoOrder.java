9b52dfb3eb771d89b9ca3e10e5cc093e944ed1a6
unknown <enzzomatrix@gmail.com>
2024-12-06 14:04:42 +1100

    upd

--- src/main/java/ru/vasilev/entity/TacoOrder.java
+++ src/main/java/ru/vasilev/entity/TacoOrder.java
@@ -1,6 +1,7 @@
 package ru.vasilev.entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
@@ -12,6 +13,10 @@ import lombok.Data;
 
 @Data
 public class TacoOrder {
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private Date placedAt = new Date();
+	
 	@NotBlank(message="Delivery name is required")
 	private String deliveryName;
 	
