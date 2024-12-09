15c536ec25d7e05429cb13f15850a71bb8ebae2a
unknown <enzzomatrix@gmail.com>
2024-12-09 17:31:23 +1100

    upd

--- src/main/java/ru/vasilev/controller/DesignTacoController.java
+++ src/main/java/ru/vasilev/controller/DesignTacoController.java
@@ -1,6 +1,5 @@
 package ru.vasilev.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,38 +27,27 @@ import ru.vasilev.repository.IngredientRepository;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
-//	private final IngredientRepository ingredientRepo;
-//	
-//	@Autowired
-//	public DesignTacoController(IngredientRepository ingredientRepo) {
-//		this.ingredientRepo = ingredientRepo;
-//	}
-//	
-	@ModelAttribute
-	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTD", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE),
-				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE),
-				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
-		
-		Type[] types = Ingredient.Type.values();
-		for(Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(),
-					filterByType(ingredients, type));
-		}
+	private final IngredientRepository ingredientRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
 	}
-
+	
 //	@ModelAttribute
 //	public void addIngredientsToModel(Model model) {
-//		List<Ingredient> ingredients = ingredientRepo.findAll();
+//		List<Ingredient> ingredients = Arrays.asList(
+//				new Ingredient("FLTD", "Flour Tortilla", Type.WRAP),
+//				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+//				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+//				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+//				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+//				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+//				new Ingredient("CHED", "Cheddar", Type.CHEESE),
+//				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+//				new Ingredient("SLSA", "Salsa", Type.SAUCE),
+//				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+//				);
 //		
 //		Type[] types = Ingredient.Type.values();
 //		for(Type type : types) {
@@ -67,6 +55,17 @@ public class DesignTacoController {
 //					filterByType(ingredients, type));
 //		}
 //	}
+
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
+		List<Ingredient> ingredients = ingredientRepo.findAll();
+		
+		Type[] types = Ingredient.Type.values();
+		for(Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(),
+					filterByType(ingredients, type));
+		}
+	}
 	
 	@ModelAttribute(name = "tacoOrder")
 	public TacoOrder order() {
--- src/main/java/ru/vasilev/converter/IngredientByIdConverter.java
+++ src/main/java/ru/vasilev/converter/IngredientByIdConverter.java
@@ -1,45 +1,43 @@
 package ru.vasilev.converter;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import ru.vasilev.entity.Ingredient;
-import ru.vasilev.entity.Ingredient.Type;
+import ru.vasilev.repository.IngredientRepository;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-	private Map<String, Ingredient> ingredientMap = new HashMap<>();
-
-	public IngredientByIdConverter() {
-		ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-		ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-		ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-		ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-		ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-		ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-		ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
-		ingredientMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-		ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
-		ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-	}
-
-	@Override
-	public Ingredient convert(String id) {
-		return this.ingredientMap.get(id);
-	}
-//	private IngredientRepository ingredientRepo;
-//	
-//	@Autowired
-//	public IngredientByIdConverter(IngredientRepository ingredientRepo) {
-//		this.ingredientRepo = ingredientRepo;
+//	private Map<String, Ingredient> ingredientMap = new HashMap<>();
+//
+//	public IngredientByIdConverter() {
+//		ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//		ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//		ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//		ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//		ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//		ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//		ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//		ingredientMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//		ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//		ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 //	}
-//	
+//
 //	@Override
 //	public Ingredient convert(String id) {
-//		return ingredientRepo.findById(id).orElse(null);
+//		return this.ingredientMap.get(id);
 //	}
+	private IngredientRepository ingredientRepo;
+	
+	@Autowired
+	public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+	
+	@Override
+	public Ingredient convert(String id) {
+		return ingredientRepo.findById(id).orElse(null);
+	}
 }
