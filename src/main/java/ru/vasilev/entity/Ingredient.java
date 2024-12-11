f01b31c5548158253b2042f38d8ef1325f1ff12d
enzzo <enzzomatrix@gmail.com>
2024-12-11 23:28:03 +1100

    upd

--- src/main/java/ru/vasilev/entity/Ingredient.java
+++ src/main/java/ru/vasilev/entity/Ingredient.java
@@ -1,26 +1,28 @@
 package ru.vasilev.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity
 @AllArgsConstructor
-@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
-public class Ingredient{	
-	
-	@Id
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Table("ingredients")
+public class Ingredient {
+
+	@PrimaryKey
 	private String id;
-	
+
 	private String name;
-	
+
 	private Type type;
+
 //	---------------------------------------------------------
-	public enum Type{
+	public enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
 	}
 }
