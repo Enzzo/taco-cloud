38886e09190df773be5f5606e35ff4bd57f44597
unknown <enzzomatrix@gmail.com>
2024-12-13 14:42:33 +1100

    upd

---
+++ 02/tacos/src/main/java/ru/vasilev/tacos/TacosApplication.java
@@ -0,0 +1,13 @@
+package ru.vasilev.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacosApplication.class, args);
+	}
+
+}
