5ec18e708f905e0609d3aa23bd3c908c07f17f31
unknown <enzzomatrix@gmail.com>
2024-12-19 17:29:55 +1100

    upd

--- 02/tacos/src/main/java/ru/vasilev/tacos/controller/DesignTacoController.java
+++ 02/tacos/src/main/java/ru/vasilev/tacos/controller/DesignTacoController.java
@@ -6,11 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import ru.vasilev.tacos.entity.Ingredient;
 import ru.vasilev.tacos.entity.Ingredient.Type;
@@ -52,9 +55,23 @@ public class DesignTacoController {
 		return new Taco();
 	}
 
-	@GetMapping("/test")
+	@GetMapping
 	public String showDesignForm() {
 		return "design";
+	}
+	
+	@PostMapping
+	public String processTaco(
+			@Valid Taco taco, Errors errors,
+			@ModelAttribute TacoOrder tacoOrder) {
+		if(errors.hasErrors()) {
+			return "design";
+		}
+		
+		tacoOrder.addTaco(taco);
+		log.info("Processing taco: {}", taco);
+		
+		return "redirect:/orders/current";
 	}
 
 	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
